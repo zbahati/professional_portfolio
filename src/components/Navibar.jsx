07c47f8b3d8b33257ea8cf1840a810be69959308
zbahati <bahatizirimwabagabo@gmail.com>
2023-12-20 10:29:51 +0200

    Add Navigation to the portfolio

---
+++ src/components/Navibar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <header className="bg-[#110F17] p-2">
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Project</li>
+          <li>Contact</li>
+        </ul>
+        <div>
+          
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
